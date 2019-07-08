26dc174be3eed75ae8d29ced9ae0fa4acf9f763f
root <root@localhost.localdomain>
2019-07-08 02:35:51 -0400

    directory code corrected

--- first-game/project.py
+++ first-game/project.py
@@ -10,8 +10,8 @@ win = pygame.display.set_mode((screen["width"], screen["height"]))
 pygame.display.set_caption("First Game")
 
 # LOADING IMAGES
-walkRight = [pygame.image.load('images\R1.png'), pygame.image.load('images\R2.png'), pygame.image.load('images\R3.png'), pygame.image.load('images\R4.png'), pygame.image.load('images\R5.png'), pygame.image.load('images\R6.png'), pygame.image.load('images\R7.png'), pygame.image.load('images\R8.png'), pygame.image.load('images\R9.png')]
-walkLeft = [pygame.image.load('images/L1.png'), pygame.image.load('images\L2.png'), pygame.image.load('images\L3.png'), pygame.image.load('images\L4.png'), pygame.image.load('images\L5.png'), pygame.image.load('images\L6.png'), pygame.image.load('images\L7.png'), pygame.image.load('images\L8.png'), pygame.image.load('images\L9.png')]
+walkRight = [pygame.image.load('images/R1.png'), pygame.image.load('images/R2.png'), pygame.image.load('images/R3.png'), pygame.image.load('images/R4.png'), pygame.image.load('images/R5.png'), pygame.image.load('images/R6.png'), pygame.image.load('images/R7.png'), pygame.image.load('images/R8.png'), pygame.image.load('images/R9.png')]
+walkLeft = [pygame.image.load('images/L1.png'), pygame.image.load('images/L2.png'), pygame.image.load('images/L3.png'), pygame.image.load('images/L4.png'), pygame.image.load('images/L5.png'), pygame.image.load('images/L6.png'), pygame.image.load('images/L7.png'), pygame.image.load('images/L8.png'), pygame.image.load('images/L9.png')]
 bg = pygame.image.load('images/bg.jpg')
 char = pygame.image.load('images/standing.png')
 
